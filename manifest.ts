d2f28e2a7c5385607170949ce80026963d681f37
Nathan Stitt <nathan@stitt.org>
2026-07-16 10:00:18 -0500

    chore(drive): require @tinycld/core >=0.4.0

--- manifest.ts
+++ manifest.ts
@@ -15,7 +15,7 @@ const manifest = {
     seed: { script: 'seed' },
     server: { package: 'server', module: 'tinycld.org/packages/drive' },
     repository: { url: 'https://github.com/tinycld/drive' },
-    peerVersions: { '@tinycld/core': '>=0.0.4 <0.1.0' },
+    peerVersions: { '@tinycld/core': '>=0.4.0 <0.5.0' },
 }
 
 export default manifest
