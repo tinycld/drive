8121bc66a8e3b5edef30c1c8a2cdb054f0d9d70f
Nathan Stitt <nathan@stitt.org>
2026-09-15 11:19:15 -0500

    chore: release v0.3.0

--- manifest.ts
+++ manifest.ts
@@ -1,7 +1,7 @@
 const manifest = {
     name: 'Drive',
     slug: 'drive',
-    version: '0.2.2',
+    version: '0.3.0',
     description: 'Cloud file storage, with WebDAV',
     routes: { directory: 'screens' },
     publicRoutes: { directory: 'public-screens' },
